90744ef95f7b54134b1abc6bdff1e24320bfab2b
linzhanguca <lzhang12@uca.edu>
2025-01-28 19:23:58 -0600

    update example scripts

--- plot_traj.py
+++ plot_traj.py
@@ -15,60 +15,11 @@ data_file = os.path.join(data_dir, "example_data.csv")  # use your own data
 ### END CODING HERE ###
 with open(data_file, newline="") as f:
     reader = csv.reader(f)
-    vel_log = tuple(reader)
-targ_lin_vels = []
-targ_ang_vels = []
-real_lin_vels = []
-real_ang_vels = []
-for vels in vel_log:
-    targ_lin_vels.append(float(vels[0]))
-    targ_ang_vels.append(float(vels[1]))
-    real_lin_vels.append(float(vels[2]))
-    real_ang_vels.append(float(vels[3]))
-
-targ_xs, targ_ys, targ_thetas = [0], [0], [0]
-real_xs, real_ys, real_thetas = [0], [0], [0]
-dt = 0.05  # seconds
-for i in range(len(vel_log)):
-    # Compute ideal trajectory
-    dx_targ = targ_lin_vels[i] * cos(targ_thetas[-1]) * dt
-    dy_targ = targ_lin_vels[i] * sin(targ_thetas[-1]) * dt
-    dth_targ = targ_ang_vels[i] * dt
-    targ_xs.append(targ_xs[-1] + dx_targ)
-    targ_ys.append(targ_ys[-1] + dy_targ)
-    targ_thetas.append(targ_thetas[-1] + dth_targ)
-    # Compute actual trajectory
-    dx_real = real_lin_vels[i] * cos(real_thetas[-1]) * dt
-    dy_real = real_lin_vels[i] * sin(real_thetas[-1]) * dt
-    dth_real = real_ang_vels[i] * dt
-    real_xs.append(real_xs[-1] + dx_real)
-    real_ys.append(real_ys[-1] + dy_real)
-    real_thetas.append(real_thetas[-1] + dth_real)
-# targ_v = []
-# real_v = []
-# err = []
-# for item in data:
-#     targ_v.append(float(item[0]))
-#     real_v.append(float(item[1]))
-#     err.append(targ_v[-1] - real_v[-1])
-
-# ### START CODING HERE ### ~ 4 lines
-# mse = None
-# ### END CODING HERE ###
-# print(f"PID Controller's Mean Squared Error: {mse}")
-
-# Plot data
-# ts = list(range(len(data)))  # create timestamps for x axis
-# for i in range(len(data)):
-#     ts[i] = 0.05 * i
-# xticks = [0] * 21
-# for i in range(21):
-#     xticks[i] = i
-# yticks = [0] * 20
-# for i in range(20):
-#     yticks[i] = i * 0.1 - 1
-
-
+    vel_data = tuple(reader)
+real_vels = []
+for vd in vel_data:
+    real_vels.append([float(vd[0]), float(vd[1])])
+# Create target velocities
 ref_vels = (
     (0.5, 0.0),
     (0.5, pi / 8),
@@ -81,10 +32,44 @@ ref_vels = (
     (-0.5, 0.0),
     (0.0, 2 * pi / 3),
 )
+targ_vels = []
+for i in range(len(real_vels)):
+    targ_vels.append(ref_vels[int(i / 40)])
+print(len(targ_vels))
 
+
+x, y, th = [0], [0], [0]
+x_hat, y_hat, th_hat = [0], [0], [0]
+dt = 0.05  # seconds
+for i in range(len(targ_vels)):
+    # Compute ideal trajectory
+    dx = targ_vels[i][0] * cos(th[-1]) * dt
+    dy = targ_vels[i][0] * sin(th[-1]) * dt
+    dth = targ_vels[i][1] * dt
+    x.append(x[-1] + dx)
+    y.append(y[-1] + dy)
+    th.append(th[-1] + dth)
+    # Compute actual trajectory
+    dx_hat = real_vels[i][0] * cos(th_hat[-1]) * dt
+    dy_hat = real_vels[i][0] * sin(th_hat[-1]) * dt
+    dth_hat = real_vels[i][1] * dt
+    x_hat.append(x_hat[-1] + dx_hat)
+    y_hat.append(x_hat[-1] + dy_hat)
+    th_hat.append(th_hat[-1] + dth_hat)
+
+# Plot data
+# ts = list(range(len(data)))  # create timestamps for x axis
+# for i in range(len(data)):
+#     ts[i] = 0.05 * i
+# xticks = [0] * 21
+# for i in range(21):
+#     xticks[i] = i
+# yticks = [0] * 20
+# for i in range(20):
+#     yticks[i] = i * 0.1 - 1
 fig, ax = plt.subplots(figsize=(10, 10))
-ax.scatter(targ_xs, targ_ys)
-ax.scatter(real_xs, real_ys)
+ax.scatter(x, y)
+ax.scatter(x_hat, y_hat)
 # ax[0].plot(ts, targ_v, "#7C878E", linewidth=2)
 # ax[0].plot(ts, real_v, "#582C83", linewidth=1.5)
 # ax[0].set_ylabel("Velocity (m/s)")
@@ -93,7 +78,7 @@ ax.scatter(real_xs, real_ys)
 # ax[0].set_xticks(xticks)
 # ax[0].set_yticks(yticks)
 # ax[0].grid()
-# ax[0].legend(["target", "actual"])
+ax.legend(["target", "actual"])
 # ax[1].plot(ts, err, "r")
 # ax[1].set_xlabel("Time Stamps (s)")
 # ax[1].set_ylabel("Error (m/s)")
